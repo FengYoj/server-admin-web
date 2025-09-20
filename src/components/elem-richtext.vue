35739142e27b8f400277ff854b4f942ff8b35b1b
Jemmy <FengYoj@outlook.com>
2025-09-20 18:51:55 +0800

    [修改] 更换富文本工具

--- src/components/elem-richtext.vue
+++ src/components/elem-richtext.vue
@@ -1,128 +1,177 @@
 <template>
-	<div class="elem-editor-box" dark-class="elem-editor-box-dark">
-        <elem-input-object :name="name" :title="title" :required="required" :getValue="getTextData"></elem-input-object>
-        <!-- 工具栏容器 -->
-        <div class="toolbar-container" :id="'toolbar-container-' + id"></div>
-        <!-- 编辑器容器 -->
-        <div class="editor" :id="'editor-' + id"></div>
+    <div class="editor-wrapper">
+        <elem-input-object :name="name" :title="title" :required="required" :getValue="getEditorData"></elem-input-object>
+        <textarea :id="'editor-' + id"></textarea>
     </div>
 </template>
 
 <script lang="ts">
 import Component, { ComponentMethods } from "@/module/component/component"
-import CKEditor from '@ckeditor/ckeditor5-build-decoupled-document'
 
-import elemInputObject from './elem-input-object.vue'
+import MessageController from "@/module/interactive/message"
+import Request from "@/module/request/request"
+import elemInputObject from "./elem-input-object.vue"
 import Utils from "@/module/utils/utils"
 
 class ElemRichTextComponent extends ComponentMethods implements ComponentEntity {
     data() {
         return {
-            id: Utils.getUuid()
+            id: Utils.getUuid(),
         }
     }
 
-	props = {
+    props = {
         name: String,
         title: String,
         required: {
             type: Boolean,
-            default: true
-        },
-        placeholder: {
-            type: String,
-            default: ""
+            default: true,
         },
         value: {
             type: String,
-            default: ""
-        }
-	}
-
-    components = {
-        elemInputObject
+            default: "",
+        },
     }
 
-	async mounted() {
-        this.initCKEditor()
+    components = {
+        elemInputObject,
+    }
+
+    async mounted() {
+        this.initEditor()
     }
 
     methods = {
-        getTextData() {
-            return this.editor.getData()
+
+        getEditorData() {
+            const editor = (window as any).tinymce.get("editor-" + this.id)
+            return editor ? editor.getContent() : ""
         },
 
-        initCKEditor() {
-            CKEditor.create(document.querySelector('#editor-' + this.id), {
-                language: 'zh-cn',
-                ckfinder: {
-                    uploadUrl: "/WebAPI/Resource/Image/Ckeditor"
-                }
-            }).then(editor => {
-                const toolbarContainer = document.querySelector('#toolbar-container-' + this.id)
-                toolbarContainer.appendChild(editor.ui.view.toolbar.element)
-                this.editor = editor
-                this.value && editor.setData(this.value)
-            }).catch(error => {
-                console.error(error)
-                // Message.error("富文本编辑器加载失败！")
+        setEditorData(html: string) {
+            const editor = (window as any).tinymce.get("editor-" + this.id)
+            if (editor) editor.setContent(html)
+        },
+
+        initEditor() {
+            // 初始化 TinyMCE
+            (window as any).tinymce.init({
+                selector: "#editor-" + this.id,
+                language: "zh-Hans",
+                license_key: "gpl",
+                height: 600,
+                menubar: true,
+                branding: false,
+                promotion: false,
+                plugins: [
+                    "advlist",
+                    "autolink",
+                    "lists",
+                    "link",
+                    "image",
+                    "charmap",
+                    "preview",
+                    "anchor",
+                    "searchreplace",
+                    "visualblocks",
+                    "code",
+                    "fullscreen",
+                    "insertdatetime",
+                    "media",
+                    "table",
+                    "help",
+                    "wordcount",
+                    "emoticons",
+                    "autosave",
+                    "quickbars",
+                    "codesample",
+                ], // 启用的插件列表
+                toolbar: [
+                    "code formatselect fontselect fontsizeselect forecolor backcolor bold italic underline strikethrough link alignment outdent indent bullist numlist blockquote subscript superscript removeformat table image media importword charmap pagebreak formatpainter cut copy undo redo restoredraft searchreplace",
+                ], // 工具栏按钮列表
+
+                image_dimensions: false,
+                media_dimensions: false,
+                content_style: `
+                    img,video {
+                        width: 100% !important;
+                        max-width: 100% !important;
+                        height: auto !important;
+                        display: block;
+                    }
+                    .mce-preview-object {
+                        width: 100% !important;
+                    }
+                `,
+
+                // ✅ 图片上传逻辑
+                images_upload_handler: async blobInfo => {
+                    const formData = new FormData()
+                    formData.append("file", blobInfo.blob(), blobInfo.filename())
+
+                    const res = await Request.post<obj>("WEB://Resource/Image/Upload", formData, {
+                        onFail: function (err) {
+                            MessageController.error(JSON.parse(err.responseText).message)
+                            return false
+                        },
+                    })
+
+                    return res.url
+                },
+
+                // 视频上传
+                file_picker_types: "media",
+                file_picker_callback: (callback: any, value: any, meta: any) => {
+                    if (meta.filetype === "media") {
+                        const input = document.createElement("input")
+                        input.type = "file"
+                        input.accept = "video/*"
+
+                        input.onchange = async () => {
+                            const file = input.files?.[0]
+                            if (!file) return
+
+                            const formData = new FormData()
+                            formData.append("file", file)
+
+                            // 上传到后端接口
+                            const res = await Request.post<obj>("WEB://Resource/Video/Upload", formData, {
+                                onFail: function (err) {
+                                    MessageController.error(JSON.parse(err.responseText).message)
+                                    return false
+                                },
+                            })
+
+                            // 回调插入视频
+                            callback(res.url, { source2: res.url, poster: "" })
+                        }
+
+                        input.click()
+                    }
+                },
+                setup: (editor: any) => {
+                    editor.on('init', () => {
+                        // 初始化完成后写入内容
+                        if (this.value) {
+                            editor.setContent(this.value)
+                        }
+                    })
+                },
             })
-        }
+        },
     }
 }
 
-export default Component.build(new ElemRichTextComponent)
+export default Component.build(new ElemRichTextComponent())
 </script>
 
-<style lang="less">
-@import (reference) "/src/style/utils.less";
-@import (reference) "/src/style/color.less";
-
-.elem-editor-box {
-	width: 100%;
-	font-size: 14px;
-	z-index: 10;
-	box-sizing: border-box;
-	border-radius: 6px;
-	transition: all 0.3s ease;
-    overflow: hidden;
-    display: block;
-
-    .border;
-
-    &:hover {
-        border-color: #b3b3b3;
-        box-shadow: 0 0 10px rgba(0,0,0,0.1);
-    }
-
-    .toolbar-container {
-        position: relative;
-        z-index: 5;
-
-        .ck-toolbar {
-            border: initial;
-            border-color: #f3f3f3;
-
-            .border-position(bottom)
-        }
-    }
-
-    .editor {
-        min-height: 300px;
-        border: initial;
-    }
+<style>
+.editor-wrapper {
+    width: 100%;
+    max-width: none;
 }
-
-.elem-editor-box-dark {
-    background: @dark_box;
-    border-color: @dark_border;
-
-    &:hover {
-        border-color: #666666;
-    }
-
-    .toolbar-container {
-        filter: brightness(0.5);
-    }
+#editor {
+    width: 100%;
+    max-width: none;
 }
 </style>
